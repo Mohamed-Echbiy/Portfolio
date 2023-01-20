d13c089b71a08154eb2536fe09919cd5dd65b7ac
Mohamed-Echbiy <echbiymed@gmail.com>
2023-01-20 15:24:35 +0100

    adding more animation

--- src/components/Herosection.tsx
+++ src/components/Herosection.tsx
@@ -1,4 +1,9 @@
-import { DownloadOutlined, GitHub, LinkedIn } from "@mui/icons-material";
+import {
+  AutoAwesome,
+  DownloadOutlined,
+  GitHub,
+  LinkedIn,
+} from "@mui/icons-material";
 import { motion } from "framer-motion";
 import React from "react";
 import TypographyHeading from "./common/TypographyHeading";
@@ -17,8 +22,31 @@ export default function Herosection() {
         animate={{ opacity: 1 }}
         transition={{ delay: 0.4, stiffness: 150 }}
       >
-        <TypographyHeading className="my-name text-center text-primary-900 mb-6">
-          <h2 className="my-name-is uppercase">mohamed echbiy</h2>
+        <TypographyHeading className="my-name text-center text-primary-900 mb-6 relative">
+          <motion.h2
+            className="my-name-is uppercase  block"
+            initial={{ textShadow: "0px 0px 0px #00000049" }}
+            animate={{ textShadow: "2px 5PX 7px #00000049" }}
+            transition={{ delay: 1.2 }}
+          >
+            mohamed echbiy
+          </motion.h2>
+          <motion.div
+            className="absolute top-5 left-3 w-2 h-2 md:w-5 md:h-5 text-yellow-400"
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ delay: 1.3 }}
+          >
+            <AutoAwesome />
+          </motion.div>
+          <motion.div
+            className="absolute -top-5 -right-3 w-2 h-2 md:w-5 md:h-5 text-yellow-400"
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ delay: 1.5 }}
+          >
+            <AutoAwesome />
+          </motion.div>
         </TypographyHeading>
 
         <div className="what-i-am mb-6">
